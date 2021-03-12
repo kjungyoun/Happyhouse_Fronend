3501b05ee4c354d8d1be0513208dfe0ec6dca3a3
kjungyoun <rlawjddbs963@naver.com>
2021-03-12 20:36:46 +0900

    bookmark 구현

--- js/chart-bar.js
+++ js/chart-bar.js
@@ -1,46 +1,52 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#292b2c';
+Chart.defaults.global.defaultFontColor = "#292b2c";
 
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["January", "천안시", "부산시", "수원시", "성남시", "서울시"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      }],
+    type: "bar",
+    data: {
+        labels: ["제주시", "천안시", "부산시", "수원시", "성남시", "서울시"],
+        datasets: [
+            {
+                label: "Revenue",
+                backgroundColor: "rgba(2,117,216,1)",
+                borderColor: "rgba(2,117,216,1)",
+                data: [4215, 5312, 6251, 7841, 9821, 14984],
+            },
+        ],
     },
-    legend: {
-      display: false
-    }
-  }
+    options: {
+        scales: {
+            xAxes: [
+                {
+                    time: {
+                        unit: "month",
+                    },
+                    gridLines: {
+                        display: false,
+                    },
+                    ticks: {
+                        maxTicksLimit: 6,
+                    },
+                },
+            ],
+            yAxes: [
+                {
+                    ticks: {
+                        min: 0,
+                        max: 15000,
+                        maxTicksLimit: 5,
+                    },
+                    gridLines: {
+                        display: true,
+                    },
+                },
+            ],
+        },
+        legend: {
+            display: false,
+        },
+    },
 });
